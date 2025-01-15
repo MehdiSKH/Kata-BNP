003a088450f1589d6d55be77e17eea9111b3b6a3
MehdiSKH <mehdiskhiri34@gmail.com>
2025-01-15 13:39:12 +0000

    Correct repository for back - adding src/main/resources to host application.properties

--- back/src/main/java/com/example/back/BackApplication.java
+++ back/src/main/java/com/example/back/BackApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackApplication {
     public static void main(String[] args) {
+        System.out.println("Application is running!");
         SpringApplication.run(BackApplication.class, args);
     }
 }
